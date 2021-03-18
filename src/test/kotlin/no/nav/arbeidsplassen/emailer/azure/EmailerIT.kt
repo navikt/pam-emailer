3e213b7443e4359d9b50aa2da7583d49d32d36a8
Tore Engvig <tore.engvig@gmail.com>
2021-03-18 11:56:37 +0100

    Rette test

--- src/test/kotlin/no/nav/arbeidsplassen/emailer/azure/EmailerIT.kt
+++ src/test/kotlin/no/nav/arbeidsplassen/emailer/azure/EmailerIT.kt
@@ -4,6 +4,7 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import no.nav.arbeidsplassen.emailer.azure.dto.MailContentType
 import no.nav.arbeidsplassen.emailer.azure.impl.EmailServiceAzure
 import org.junit.jupiter.api.Test
+import java.util.*
 
 @MicronautTest
 class EmailerIT(private val emailServiceAzure: EmailServiceAzure) {
@@ -11,6 +12,6 @@ class EmailerIT(private val emailServiceAzure: EmailServiceAzure) {
     @Test
     fun sendEmailAzure() {
         emailServiceAzure.sendSimpleMessage("recipient@somewhere.com", "Dette er en test",
-            MailContentType.TEXT, "Dette er en test");
+            MailContentType.TEXT, "Dette er en test", UUID.randomUUID().toString());
     }
 }
