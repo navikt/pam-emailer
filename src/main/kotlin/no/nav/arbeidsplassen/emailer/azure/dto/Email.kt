89708696692185ad875b40116f3b3011e0595e13
Christoffer Karlsson <christoffer.karlsson@nav.no>
2022-04-11 10:31:57 +0200

    Add Attachment class to Azure Email

--- src/main/kotlin/no/nav/arbeidsplassen/emailer/azure/dto/Email.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/emailer/azure/dto/Email.kt
@@ -1,8 +1,12 @@
 package no.nav.arbeidsplassen.emailer.azure.dto
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Email(val message:Message, val saveToSentItems:Boolean=false)
 
-data class Message(val subject: String, val body: Body, val toRecipients: List<Recipient> = listOf<Recipient>())
+data class Message(val subject: String, val body: Body, val toRecipients: List<Recipient> = listOf<Recipient>(),
+                   @JsonInclude(JsonInclude.Include.NON_EMPTY) val attachments: List<Attachment>)
 
 data class Body(val contentType: MailContentType = MailContentType.TEXT, val content: String)
 
@@ -10,6 +14,13 @@ data class Recipient(val emailAddress: Address)
 
 data class Address(val address: String)
 
+data class Attachment(
+    val name: String,
+    val contentType: String,
+    val contentBytes: String,
+    @JsonProperty("@odata.type")
+    val odataType: String = "#microsoft.graph.fileAttachment",
+    )
 
 enum class MailContentType {
     TEXT,
