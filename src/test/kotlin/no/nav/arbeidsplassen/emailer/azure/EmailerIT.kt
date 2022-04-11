5f9732e066d9e4973d27e8ec57a886f700c9b4e1
Christoffer Karlsson <christoffer.karlsson@nav.no>
2022-04-11 10:34:05 +0200

    Add an integration test for sending email with attachment

--- src/test/kotlin/no/nav/arbeidsplassen/emailer/azure/EmailerIT.kt
+++ src/test/kotlin/no/nav/arbeidsplassen/emailer/azure/EmailerIT.kt
@@ -1,6 +1,7 @@
 package no.nav.arbeidsplassen.emailer.azure
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import no.nav.arbeidsplassen.emailer.azure.dto.Attachment
 import no.nav.arbeidsplassen.emailer.azure.dto.MailContentType
 import no.nav.arbeidsplassen.emailer.azure.impl.EmailServiceAzure
 import org.junit.jupiter.api.Test
@@ -13,5 +14,14 @@ class EmailerIT(private val emailServiceAzure: EmailServiceAzure) {
     fun sendEmailAzure() {
         emailServiceAzure.sendSimpleMessage("recipient@somewhere.com", "Dette er en test",
             MailContentType.TEXT, "Dette er en test", UUID.randomUUID().toString());
+    }
+
+    @Test
+    fun sendEmailAzureWithAttachment() {
+        val encodeToByteArray: ByteArray = "Hei!".encodeToByteArray()
+        val attachment = Attachment("test.txt", "plain/text", Base64.getEncoder().encodeToString(encodeToByteArray))
+
+        emailServiceAzure.sendSimpleMessage("recipient@somewhere.com", "Dette er en test",
+            MailContentType.TEXT, "Dette er en test", UUID.randomUUID().toString(), listOf(attachment));
     }
 }
