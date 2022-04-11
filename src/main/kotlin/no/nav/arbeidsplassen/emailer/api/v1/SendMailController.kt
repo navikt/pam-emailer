76ad28f640b881f62f4362431c3b6f38a2f50e07
Christoffer Karlsson <christoffer.karlsson@nav.no>
2022-04-11 10:35:49 +0200

    Map attachment dtos in controller to be sent with email

--- src/main/kotlin/no/nav/arbeidsplassen/emailer/api/v1/SendMailController.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/emailer/api/v1/SendMailController.kt
@@ -3,10 +3,10 @@ package no.nav.arbeidsplassen.emailer.api.v1
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
+import no.nav.arbeidsplassen.emailer.azure.dto.Attachment
 import no.nav.arbeidsplassen.emailer.azure.dto.MailContentType
 import no.nav.arbeidsplassen.emailer.azure.impl.EmailServiceAzure
 import org.slf4j.LoggerFactory
@@ -24,8 +24,9 @@ class SendMailController(private val emailServiceAzure: EmailServiceAzure) {
     fun sendMail(@Body email: EmailDTO): HttpResponse<String> {
         val id = email.identifier ?: UUID.randomUUID().toString()
         LOG.info("Got email request with id: ${id}")
+        val attachments = email.attachments.map { Attachment(it.name, it.contentType, it.base64Content) }
         emailServiceAzure.sendSimpleMessage(email.recipient, email.subject,
-            MailContentType.valueOf(email.type),email.content, id)
+            MailContentType.valueOf(email.type),email.content, id, attachments)
         return HttpResponse.created("Created")
     }
 }
