e6d070af2fb6f2cfdd80b4b97759d38a341b8ced
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-12-02 19:50:19 +0100

    Send email immediately after received if there's quota left, else, save it to outbox. Also set status to failed when email has failed being sent.

--- src/main/kotlin/no/nav/arbeidsplassen/emailer/sendmail/EmailService.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/emailer/sendmail/EmailService.kt
@@ -1,13 +1,47 @@
 package no.nav.arbeidsplassen.emailer.sendmail
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.arbeidsplassen.emailer.azure.impl.EmailServiceAzure
+import no.nav.arbeidsplassen.emailer.azure.impl.SendMailException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
 
 @Service
-class EmailService(private val emailServiceAzure: EmailServiceAzure) {
+class EmailService(
+    private val emailRepository: OutboxEmailRepository,
+    private val emailServiceAzure: EmailServiceAzure,
+    private val objectMapper: ObjectMapper
+) {
+    private val limitHandler = LimitHandler(emailRepository)
 
-    fun sendEmail(email: Email, emailId: String, priority: Priority) {
-        emailServiceAzure.sendMail(email, emailId)
+    @Transactional
+    fun sendNewEmail(email: Email, emailId: String, priority: Priority) {
+        val outboxEmail = OutboxEmail.newOutboxEmail(emailId, priority, objectMapper.writeValueAsString(email))
+
+        if (limitHandler.canSendEmailNow()) {
+            try {
+                emailServiceAzure.sendMail(email, emailId)
+                outboxEmail.successfullySent()
+            } catch (e: SendMailException) {
+                outboxEmail.failedToSend()
+            }
+        }
+
+        emailRepository.create(outboxEmail)
+    }
+
+    @Transactional
+    fun sendEmail(outboxEmail: OutboxEmail) {
+        try {
+            val email = objectMapper.readValue(outboxEmail.payload, Email::class.java)
+            emailServiceAzure.sendMail(email, outboxEmail.emailId)
+            outboxEmail.successfullySent()
+        } catch (e: SendMailException) {
+            outboxEmail.failedToSend()
+        }
+
+        emailRepository.update(outboxEmail)
     }
 
 }
