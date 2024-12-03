9a00786620bab948a2fa08c38d3394237eaf5476
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-12-03 13:50:14 +0100

    Schedule sending of pending and failed e-mails

--- src/main/kotlin/no/nav/arbeidsplassen/emailer/sendmail/LimitHandler.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/emailer/sendmail/LimitHandler.kt
@@ -1,13 +1,25 @@
 package no.nav.arbeidsplassen.emailer.sendmail
 
+import org.springframework.stereotype.Service
 import kotlin.math.min
 
+@Service
 class LimitHandler(private val emailRepository: OutboxEmailRepository) {
     companion object {
         const val MAX_EMAILS_PER_HOUR = 2_850   // Actually 3000, but use a 5 % buffer to avoid hitting the limit
-        const val PENDING_EMAIL_BATCH_SIZE = 20
-        const val RETRY_EMAIL_BATCH_SIZE = 20
-        const val MAX_RETRIES_PER_EMAIL = 25
+
+        // 10 every 10 seconds = max 3600 per hour
+        const val PENDING_EMAIL_BATCH_SIZE = 10
+        const val PENDING_EMAIL_CRON = "*/10 * * * * *"
+        const val PENDING_EMAIL_LOCK_AT_LEAST_FOR = "PT8S"
+        const val PENDING_EMAIL_LOCK_AT_MOST_FOR = "PT5M"
+
+        // 100 every 5 minutes = max 1200 per hour
+        const val RETRY_EMAIL_BATCH_SIZE = 100
+        const val RETRY_EMAIL_CRON = "0 */5 * * * *"
+        const val RETRY_EMAIL_LOCK_AT_LEAST_FOR = "PT4M"
+        const val RETRY_EMAIL_LOCK_AT_MOST_FOR = "PT20M"
+        const val MAX_RETRIES = 15
     }
 
     fun canSendEmailNow(): Boolean {
---
+++ src/main/kotlin/no/nav/arbeidsplassen/emailer/sendmail/ScheduledOutboxEmailSender.kt
@@ -0,0 +1,69 @@
+package no.nav.arbeidsplassen.emailer.sendmail
+
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
+import no.nav.arbeidsplassen.emailer.sendmail.LimitHandler.Companion.PENDING_EMAIL_CRON
+import no.nav.arbeidsplassen.emailer.sendmail.LimitHandler.Companion.PENDING_EMAIL_LOCK_AT_LEAST_FOR
+import no.nav.arbeidsplassen.emailer.sendmail.LimitHandler.Companion.PENDING_EMAIL_LOCK_AT_MOST_FOR
+import no.nav.arbeidsplassen.emailer.sendmail.LimitHandler.Companion.RETRY_EMAIL_CRON
+import no.nav.arbeidsplassen.emailer.sendmail.LimitHandler.Companion.RETRY_EMAIL_LOCK_AT_LEAST_FOR
+import no.nav.arbeidsplassen.emailer.sendmail.LimitHandler.Companion.RETRY_EMAIL_LOCK_AT_MOST_FOR
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Service
+class ScheduledOutboxEmailSender(
+    private val emailService: EmailService,
+    private val limitHandler: LimitHandler,
+    private val outboxEmailRepository: OutboxEmailRepository
+) {
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ScheduledOutboxEmailSender::class.java)
+    }
+
+    @Scheduled(cron = PENDING_EMAIL_CRON)
+    @SchedulerLock(
+        name = "sendPendingEmails",
+        lockAtLeastFor = PENDING_EMAIL_LOCK_AT_LEAST_FOR,
+        lockAtMostFor = PENDING_EMAIL_LOCK_AT_MOST_FOR
+    )
+    fun sendPendingEmails() {
+        val numberOfEmailsToSend = limitHandler.emailsToSend()
+
+        if (numberOfEmailsToSend == 0) {
+            LOG.info("No quota left for sending emails")
+            return
+        }
+
+        val emails = outboxEmailRepository.findPendingSortedByCreated(numberOfEmailsToSend)
+
+        if (emails.isNotEmpty()) {
+            LOG.info("Sending ${emails.size} emails (max batch size was $numberOfEmailsToSend)")
+
+            emails.forEach {
+                emailService.sendEmail(it)
+            }
+        }
+    }
+
+    @Scheduled(cron = RETRY_EMAIL_CRON)
+    @SchedulerLock(name = "retryFailedEmails", lockAtLeastFor = RETRY_EMAIL_LOCK_AT_LEAST_FOR, lockAtMostFor = RETRY_EMAIL_LOCK_AT_MOST_FOR)
+    fun retryFailedEmails() {
+        val numberOfEmailsToSend = limitHandler.emailsToRetry()
+
+        if (numberOfEmailsToSend == 0) {
+            LOG.info("No quota left for retrying failed emails")
+            return
+        }
+
+        val emails = outboxEmailRepository.findFailedSortedByUpdated(numberOfEmailsToSend)
+
+        if (emails.isNotEmpty()) {
+            LOG.info("Retrying ${emails.size} failed emails (max batch size was $numberOfEmailsToSend)")
+
+            emails.forEach {
+                emailService.sendEmail(it)
+            }
+        }
+    }
+}
