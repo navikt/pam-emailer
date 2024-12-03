ca4754733f86d0fd3c2d661767018b3c02fb5f2f
Christoffer Karlsson <christoffer.karlsson@nav.no>
2024-12-03 14:36:40 +0100

    Clean database before each repository test, since some of them are asserting on count value

--- src/test/kotlin/no/nav/arbeidsplassen/emailer/sendmail/OutboxEmailRepositoryTest.kt
+++ src/test/kotlin/no/nav/arbeidsplassen/emailer/sendmail/OutboxEmailRepositoryTest.kt
@@ -2,9 +2,11 @@ package no.nav.arbeidsplassen.emailer.sendmail
 
 import no.nav.arbeidsplassen.emailer.PostgresTestDatabase
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
 import java.time.OffsetDateTime
 import java.time.temporal.ChronoUnit
 import java.util.*
@@ -14,7 +16,15 @@ import kotlin.test.assertEquals
 class OutboxEmailRepositoryTest : PostgresTestDatabase() {
 
     @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    @Autowired
     lateinit var outboxEmailRepository: OutboxEmailRepository
+
+    @BeforeEach
+    fun cleanDatabase() {
+        jdbcTemplate.update("DELETE FROM outbox_email")
+    }
 
     @Test
     fun `E-mails are stored and read correctly`() {
